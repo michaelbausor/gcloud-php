af67eff973e1440ecc5c49d4a8439ba4b415db15
Michael Bausor <michaelbausor@google.com>
2018-03-31 17:03:47 -0700

    Update Video V1

--- VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
+++ VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2017 Google LLC
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,15 +37,12 @@ use Google\ApiCore\LongRunning\OperationsClient;
 use Google\ApiCore\OperationResponse;
 use Google\ApiCore\RetrySettings;
 use Google\ApiCore\Transport\TransportInterface;
-use Google\Auth\CredentialsLoader;
 use Google\Cloud\VideoIntelligence\V1\AnnotateVideoProgress;
 use Google\Cloud\VideoIntelligence\V1\AnnotateVideoRequest;
 use Google\Cloud\VideoIntelligence\V1\AnnotateVideoResponse;
 use Google\Cloud\VideoIntelligence\V1\Feature;
 use Google\Cloud\VideoIntelligence\V1\VideoContext;
 use Google\LongRunning\Operation;
-use Grpc\Channel;
-use Grpc\ChannelCredentials;
 
 /**
  * Service Description: Service that implements Google Cloud Video Intelligence API.
@@ -111,11 +108,6 @@ class VideoIntelligenceServiceGapicClient
     const SERVICE_ADDRESS = 'videointelligence.googleapis.com';
 
     /**
-     * The default port of the service.
-     */
-    const DEFAULT_SERVICE_PORT = 443;
-
-    /**
      * The name of the code generator, to be included in the agent header.
      */
     const CODEGEN_NAME = 'gapic';
@@ -125,6 +117,13 @@ class VideoIntelligenceServiceGapicClient
      */
     const CODEGEN_VERSION = '0.0.5';
 
+    /**
+     * The default scopes required by the service.
+     */
+    public static $serviceScopes = [
+        'https://www.googleapis.com/auth/cloud-platform',
+    ];
+
     private $operationsClient;
 
     private static function getClientDefaults()
@@ -132,10 +131,7 @@ class VideoIntelligenceServiceGapicClient
         return [
             'serviceName' => self::SERVICE_NAME,
             'serviceAddress' => self::SERVICE_ADDRESS,
-            'port' => self::DEFAULT_SERVICE_PORT,
-            'scopes' => [
-                'https://www.googleapis.com/auth/cloud-platform',
-            ],
+            'scopes' => self::$serviceScopes,
             'clientConfigPath' => __DIR__.'/../resources/video_intelligence_service_client_config.json',
             'restClientConfigPath' => __DIR__.'/../resources/video_intelligence_service_rest_client_config.php',
             'descriptorsConfigPath' => __DIR__.'/../resources/video_intelligence_service_descriptor_config.php',
@@ -184,51 +180,30 @@ class VideoIntelligenceServiceGapicClient
      * @param array $options {
      *                       Optional. Options for configuring the service API wrapper.
      *
-     *     @type string $serviceAddress The domain name of the API remote host.
-     *                                  Default 'videointelligence.googleapis.com'.
-     *     @type mixed $port The port on which to connect to the remote host. Default 443.
-     *     @type Channel $channel
-     *           A `Channel` object. If not specified, a channel will be constructed.
-     *           NOTE: This option is only valid when utilizing the gRPC transport.
-     *     @type ChannelCredentials $sslCreds
-     *           A `ChannelCredentials` object for use with an SSL-enabled channel.
-     *           Default: a credentials object returned from
-     *           \Grpc\ChannelCredentials::createSsl().
-     *           NOTE: This option is only valid when utilizing the gRPC transport. Also, if the $channel
-     *           optional argument is specified, then this argument is unused.
-     *     @type bool $forceNewChannel
-     *           If true, this forces gRPC to create a new channel instead of using a persistent channel.
-     *           Defaults to false.
-     *           NOTE: This option is only valid when utilizing the gRPC transport. Also, if the $channel
-     *           optional argument is specified, then this option is unused.
-     *     @type CredentialsLoader $credentialsLoader
-     *           A CredentialsLoader object created using the Google\Auth library.
-     *     @type string[] $scopes A string array of scopes to use when acquiring credentials.
-     *                          Defaults to the scopes for the Cloud Video Intelligence API.
-     *     @type string $clientConfigPath
-     *           Path to a JSON file containing client method configuration, including retry settings.
-     *           Specify this setting to specify the retry behavior of all methods on the client.
-     *           By default this settings points to the default client config file, which is provided
-     *           in the resources folder. The retry settings provided in this option can be overridden
-     *           by settings in $retryingOverride
-     *     @type array $retryingOverride
-     *           An associative array in which the keys are method names (e.g. 'createFoo'), and
-     *           the values are retry settings to use for that method. The retry settings for each
-     *           method can be a {@see Google\ApiCore\RetrySettings} object, or an associative array
-     *           of retry settings parameters. See the documentation on {@see Google\ApiCore\RetrySettings}
-     *           for example usage. Passing a value of null is equivalent to a value of
-     *           ['retriesEnabled' => false]. Retry settings provided in this setting override the
-     *           settings in $clientConfigPath.
-     *     @type callable $authHttpHandler A handler used to deliver PSR-7 requests specifically
-     *           for authentication. Should match a signature of
-     *           `function (RequestInterface $request, array $options) : ResponseInterface`.
-     *     @type callable $httpHandler A handler used to deliver PSR-7 requests. Should match a
-     *           signature of `function (RequestInterface $request, array $options) : PromiseInterface`.
-     *           NOTE: This option is only valid when utilizing the REST transport.
+     *     @type string $serviceAddress The address of the API remote host. Must be formatted as "<uri>:<port>".
+     *           Default 'videointelligence.googleapis.com'.
+     *     @type array $keyFile The contents of the service account credentials
+     *           .json file retrieved from the Google Developer's Console.
+     *           Ex: `json_decode(file_get_contents($path), true)`.
+     *     @type string $keyFilePath The full path to your service account
+     *           credentials .json file retrieved from the Google Developers
+     *           Console.
+     *     @type string $clientConfig Array containing client method configuration,
+     *           including retry settings. If this argument is specified, the value
+     *           of the $clientConfigPath option will be ignored.
+     *     @type string $clientConfigPath Path to a JSON file containing client method
+     *           configuration, including retry settings. Specify this setting to
+     *           specify the retry behavior of all methods on the client.
+     *           If the $clientConfig option is specified, this will be ignored.
+     *     @type bool $disableRetries Determines whether or not retries defined
+     *           by the client configuration should be disabled. Defaults to `false`.
      *     @type string|TransportInterface $transport The transport used for executing network
      *           requests. May be either the string `rest` or `grpc`. Additionally, it is possible
      *           to pass in an already instantiated transport. Defaults to `grpc` if gRPC support is
      *           detected on the system.
+     *     @type array $transportConstructionOptions An array of options to be passed to
+     *           TransportFactory::build. If the $transport option specifies an existing
+     *           TransportInterface object, then this argument will be ignored.
      * }
      * @experimental
      */
